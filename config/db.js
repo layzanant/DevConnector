74014d056f237652073753fd52926192b0449db8
Anant Jain <jainanant36@gmail.com>
2024-06-11 17:06:35 -0400

    Integrated JWT auth, created verification middleware and added user routes and profile routes

--- config/db.js
+++ config/db.js
@@ -3,14 +3,14 @@ const config = require('config');
 
 const db = config.get('mongoURI');
 
-const connectDB = async ()=> {
-    try{
-        await mongoose.connect(db);
-        console.log('DB Connected....');
-    } catch(err) {
-        console.log(err.message);
-        process.exit(1); //Exit process with failure
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log('DB Connected....');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); //Exit process with failure
+  }
+};
 
 module.exports = connectDB;
